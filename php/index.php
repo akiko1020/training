7a17c421cf0afb62485ceb08a43f7dc15e3dac6d
kadoyaakiko <akiko@kadoyaakiko-no-MacBook-Pro.local>
2015-11-07 10:08:22 +0900

    サンプルページの作成。

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<head></head>
+		<main>
+			<?php
+				print <<< EOS
+今日の優理はわがままで<br>
+困っちゃいますね。<br>
+電車のDVDを見ています。<br>
+EOS;
+			?>
+		</main>
+		<footer></footer>
+	</body>
+</html>
